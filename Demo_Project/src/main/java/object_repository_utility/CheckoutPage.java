fb06bf4e1cd5ea641999fd8aaa89c3a8b78a4dbd
Ajith Chowdary <Ajith Chowdary@Vandhana-Ajith>
2026-09-21 17:34:19 +0530

    Added Element in checkoutpage in OR

--- Demo_Project/src/main/java/object_repository_utility/CheckoutPage.java
+++ Demo_Project/src/main/java/object_repository_utility/CheckoutPage.java
@@ -1,5 +1,19 @@
 package object_repository_utility;
 
-public class CheckoutPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CheckoutPage 
+{
+	WebDriver driver;
+	public CheckoutPage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(linkText="CheckoutPage")
+    private WebElement CheckoutPagelink;
 
 }
