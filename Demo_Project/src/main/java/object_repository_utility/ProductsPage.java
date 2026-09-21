6fdffe538bf182dce091aefa8b387eea2a45408c
Ajith Chowdary <Ajith Chowdary@Vandhana-Ajith>
2026-09-21 15:46:16 +0530

    Added An Element

--- Demo_Project/src/main/java/object_repository_utility/ProductsPage.java
+++ Demo_Project/src/main/java/object_repository_utility/ProductsPage.java
@@ -1,6 +1,8 @@
 package object_repository_utility;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ProductsPage 
@@ -10,6 +12,9 @@ public class ProductsPage
 	{
 		PageFactory.initElements(driver, this);
 	}
+	
+	@FindBy(linkText = "Products")
+	private WebElement productlink;
 	
 
 }
