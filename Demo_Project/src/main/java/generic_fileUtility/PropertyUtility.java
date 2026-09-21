3397d5921c51f100c5232e51a36f06b4471d6331
Benny <Benny@Benjamin.mshome.net>
2026-09-21 16:31:08 +0530

    new property file

---
+++ Demo_Project/src/main/java/generic_fileUtility/PropertyUtility.java
@@ -0,0 +1,18 @@
+package generic_fileUtility;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertyUtility {
+
+	FileInputStream fis;
+
+	public String readDataFromProperty(String key) throws Exception {
+		fis = new FileInputStream("./src/test/resources/CommonData.properties");
+		Properties p = new Properties();
+		p.load(fis);
+		return p.getProperty(key);
+	}
+}
+
+
